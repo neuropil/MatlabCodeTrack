75f1c3eb6a01ec76b1b7ddd5240214d043e30f20
John Thompson <john.a.thompson@cuanschutz.edu>
2021-06-19 17:21:36 -0600

    update

---
+++ ideaforTrack.m
@@ -0,0 +1,26 @@
+
+
+
+% Start of day
+
+% Loop through files open in Editor
+
+% 1. Diary file name for file
+
+% 2. Type filename
+
+% 3. Close diary
+
+% 4. Read in fopen and save a two column cell array/ table with filename
+% and lines of code with top two lines and bottom line stripped out
+
+
+% 5. At end of day rerun and calculate difference in line number and line
+% length
+
+% t = matlab.desktop.editor.getAll
+% t2 = t(1,1).Text
+% t3 = strsplit(t2,'\n')
+% t4 ====== find last empty with cellfun
+
+% Compare line length and line number 
